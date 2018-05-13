1de776f767187c929946572c7bed7471640b1ca7
uros.purtic <uros.purtic@gmail.com>
2018-05-13 20:05:26 +0200

    slicing string on vuew all page

--- src/pages/AppPosts.vue
+++ src/pages/AppPosts.vue
@@ -7,15 +7,13 @@
                 <th scope="col">#</th>
                 <th scope="col">Title</th>
                 <th scope="col">Text</th>
-                <th scope="col">Created at</th>
             </tr>
             </thead>
             <tbody>
             <tr v-for="(post,key) in posts" :key ='key'>
                 <th scope="row">{{post.id}}</th>
                 <td>{{ post.title}}</td>
-                <td>{{ post.text}}</td>
-                <td>{{ post.createdAt}}</td>
+                <td>{{ post.text.slice(0,15)}}...</td>
             </tr>
             </tbody>
         </table>
